85a9fad68b54b42cb2dd5a87ff2b93dd463d73ce
davide3011 <davide.grilli@outlook.com>
2026-02-16 00:05:43 +0100

    Replace bitcoin URI scheme with palladium in donation flow and add palladium URI support

--- electrum/gui/qt/main_window.py
+++ electrum/gui/qt/main_window.py
@@ -858,7 +858,7 @@ class ElectrumWindow(QMainWindow, MessageBoxMixin, Logger, QtEventListener):
         if d:
             self.show_send_tab()
             host = self.network.get_parameters().server.host
-            self.handle_payment_identifier('bitcoin:%s?message=donation for %s' % (d, host))
+            self.handle_payment_identifier('palladium:%s?message=donation for %s' % (d, host))
         else:
             self.show_error(_('No donation address for this server'))
 
